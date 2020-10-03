5ff0631dcdfc86e305069e7baf8b380e3dfbc111
vivekvaradarajan1980 <70354824+vivekvaradarajan1980@users.noreply.github.com>
2020-10-03 19:31:37 -0400

    Add files via upload
    I had to redo the exit function as a custom function because previously it wasnt closing out the gui window....

--- wmitest.py
+++ wmitest.py
@@ -10,10 +10,18 @@ attribute iterator
 c=wmi.WMI().classname()[0]
 """
 
+"""enter the class name for the wmi query"""
+
+
 e=raw_input('Enter classname ')
 options=eval('wmi.WMI().'+e+'()[0]')
 optionchosen=StringVar()
+
+""" create a list of options from the wmi class that user entered"""
 optionchosen.set(list(options.properties)[0])
+
+
+""" here is your pop up menu"""
 popupmenu=OptionMenu(root,optionchosen,*list(options.properties))
 popupmenu.pack()
 
@@ -24,8 +32,12 @@ def print_class():
     classchosen=str(optionchosen.get())
     print getattr(options,classchosen,'') 
 
+def myexit():
+    root.destroy()
+    exit()
 b=Button(root,text="select class",command=print_class)
-eb=Button(root,text='quit program',command=exit)
+
+eb=Button(root,text='quit program',command=myexit)
 b.pack()
 eb.pack()
 

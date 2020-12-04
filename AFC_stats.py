938c38a6547f2a9679633d2add9dfb9bed1f0c63
vivekvaradarajan1980 <70354824+vivekvaradarajan1980@users.noreply.github.com>
2020-12-04 18:24:18 -0500

    Add files via upload
    Pandas based program to interac with AFC surveymonkey data..

---
+++ AFC_stats.py
@@ -0,0 +1,30 @@
+from pandas import *
+
+"extract data frame from file, I renamed the file for convinience !!"
+
+data=read_excel('c:\\Users\\vivek\\Desktop\\AFCsurvey.xlsx')
+
+
+"How many are part of softwawre factory"
+
+" here is where I use filters to parse on one or more columns and get the numbers !!!, how many said what bla bla "
+" start of by eliminating the null value from receipient ID"
+
+
+AIfilter=(data.iloc[:,12].str.contains(('AI')))
+softwarefac=(data.iloc[:,11]=='Software Factory')
+nonull_rid=(data.iloc[:,0].notnull())
+dontknowotherprogram=(data.iloc[:,13].str.contains(('I did not')))
+
+"total applied for software factory only"
+print(data[softwarefac & nonull_rid].shape)
+
+"total applied for AI"
+print(data[AIfilter & nonull_rid].shape)
+
+"total applied for both"
+print(data[(data.iloc[:,11]=='Software Factory') & (data.iloc[:,12].str.contains(('AI'))) & (data.iloc[:,0].notnull())].shape)
+
+"total applied to Software fac but didnt know about other program...."
+print(data[softwarefac & nonull_rid &dontknowotherprogram].shape)
+
